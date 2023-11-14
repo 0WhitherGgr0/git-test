25609b12e7d7249e682759ca37a75db25e4f321c
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2023-11-13 20:42:05 -0500

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Este es un archivo de script básico, puedes agregar tu lógica aquí.
+
+document.addEventListener('DOMContentLoaded', function() {
+    // El código que se ejecutará después de que la página se cargue completamente
+    console.log('La página se ha cargado completamente.');
+    
+    // Puedes agregar más funciones y lógica aquí.
+});
